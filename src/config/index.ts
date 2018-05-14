2a0b5e1b12707b5e035c36968632b2730161fc98
Zachery Johnson <zacheryjohnson13@gmail.com>
2018-05-14 16:30:43 -0700

    Pushing to master aww yiss

---
+++ src/config/index.ts
@@ -0,0 +1,18 @@
+
+import {environment as prod } from './environment.prod'
+import {environment as dev } from './environment.dev'
+
+const config = {
+    // Add common config values here
+}
+
+const configEnv = prod
+// const configEnv = process.env.NODE_ENV === 'production'
+// ? prod
+// : dev
+
+export default {
+// Add common config values here
+  ...config,
+  ...configEnv
+}
