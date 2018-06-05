94657e16487c594d6aa4563d2f5d3440d75e9263
Zachery Johnson <zacheryjohnson13@gmail.com>
2018-06-05 14:54:42 -0700

    Added all the backend bullshit that makes Typescript play nicely with Mocha

---
+++ test/post.test.ts
@@ -0,0 +1,12 @@
+import "mocha";
+import * as chai from "chai";
+chai.should();
+
+import * as PostComponent from "../src/components/post";
+
+describe("Post Actions", function(){
+    it("should create an action on post create");
+    it("should create an action on post delete");
+    it("should create an action on post favorite");
+    it("should create an action on post comment");
+});
