5374c327afaa96a593c9da188df7d7f0a3c9e77a
Zachery Johnson <zacheryjohnson13@gmail.com>
2018-06-09 14:54:47 -0700

    Finished friend revamp

--- src/data/firestoreClient/services/friends/FriendService.ts
+++ src/data/firestoreClient/services/friends/FriendService.ts
@@ -2,8 +2,17 @@ import { db } from "data/firestoreClient";
 import { IFriendService } from "./IFriendService";
 
 import * as moment from "moment";
+import { provider } from "socialEngine";
+import { IUserTieService } from "core/services/circles";
+import { Notification } from "src/core/domain/notifications";
+import { INotificationService } from "core/services/notifications";
+import { SocialProviderTypes } from "core/socialProviderTypes";
 
-const buildBase64Set = () => {
+/**
+ * Creates an array of 64 characters: 0-9, A-Z, a-z, '-', and '_'.
+ * @return an array of base 64 characters
+ */
+function buildBase64Set(): string[] {
     let set = [] as string[];
     // Add numbers 0-9
     for (let i = 0; i < 10; i++) {
@@ -24,18 +33,56 @@ const buildBase64Set = () => {
     set.push("_");
 
     return set;
-};
+}
 
 const BASE_64_SET = buildBase64Set();
+
+/**
+ * Retrieves the ID of a user's default "following" circle.
+ * @param {string} userId the ID of the user
+ * @return the ID of the user's default "following" circle within a Promise
+ */
+function getDefaultCircle(userId: string): Promise<string> {
+    return new Promise<string>( async (resolve, reject) => {
+        const userCircles = await db.collection("users").doc(userId)
+            .collection("circles").get();
+
+        userCircles.forEach(circle => {
+            return resolve(circle.id);
+        });
+
+        return reject("Could not load default circle.");
+    });
+}
+
+/**
+ * Gets the full name of a user given their user ID.
+ * @param {string} userId the ID of the user
+ * @return the full name of the user within a Promise
+ */
+function getFriendName(userId: string): Promise<string> {
+    return new Promise<string>( async (resolve, reject) => {
+        const user = await db.collection("userInfo").doc(userId).get();
+
+        if (user == null) {
+            return reject("Could not find user " + userId);
+        }
+
+        resolve(user.get("fullName"));
+    });
+}
+
+const userTieService: IUserTieService = provider.get<IUserTieService>(SocialProviderTypes.UserTieService);
+const notificationService: INotificationService = provider.get<INotificationService>(SocialProviderTypes.NotificationService);
 
 export class FriendService implements IFriendService {
     public sendFriendRequest = (userId: string, friendId: string) => {
         return new Promise<string>( async (resolve, reject) => {
             // Verify that the friend ID belongs to a valid user
             const user = await db.collection("userInfo").doc(friendId).get();
+
             // If user does not exist, error
             if (!user.exists) {
-                console.log("User ID " + friendId + " does not exist.");
                 reject("User ID " + friendId + " does not exist.");
             }
 
@@ -45,24 +92,71 @@ export class FriendService implements IFriendService {
                 // XOR the strings to get a unique identifier
                 const xorResult = userId.charCodeAt(i) ^ friendId.charCodeAt(i);
 
+                // The value will be between 0 - 256 (since chars are 8 bits)
+                // Since we use base 64 representation, set it to 0 - 256 instead
                 const value = xorResult % 64;
-                const remainder = Math.floor(xorResult / 64);
 
-                requestId += BASE_64_SET[value] + BASE_64_SET[remainder];
+                // To keep the uniqueness of the identifier, we need to distinguish
+                // a result of 0 from 64 from 128 from 192
+                // so we'll store the multiplicity immediately after the character
+                const multiplicity = Math.floor(xorResult / 64);
+
+                // As an example, if the xorResult was 87,
+                // the value would be 23 and multiplicity 1
+                // so the requestId would have BASE_64_SET[23] and 1 appended to it
+                requestId += BASE_64_SET[value] + multiplicity;
             }
 
-            console.log(requestId);
             db.collection("friendRequests").doc(requestId).get()
-            .then(request => {
+            .then(async (request) => {
+                const userDefaultCircle = await getDefaultCircle(userId);
+
                 if (request.exists) {
-                    console.log("Request exists!");
+                    // Make sure the friend request sender isn't the user
+                    // (ie a person can't send a request twice to force an accept)
+                    if (userId === request.data()!.senderId) {
+                        return resolve();
+                    }
+
+                    // Fetch the friend's default circle
+                    const friendDefaultCircle = await getDefaultCircle(friendId);
+
                     // Make them friends
+                    userTieService.tieUseres({userId: userId}, {userId: friendId}, [userDefaultCircle]);
+                    userTieService.tieUseres({userId: friendId}, {userId: userId}, [friendDefaultCircle]);
+
+                    // Create notification objects for each user
+                    const userFullName = await getFriendName(userId);
+                    const friendFullName = await getFriendName(friendId);
+                    const userNotification: Notification = {
+                        isSeen: false,
+                        description: `You are now friends with ${friendFullName}.`,
+                        url: `/${friendId}`,
+                        notifierUserId: friendId,
+                        notifyRecieverUserId: userId
+                    };
+
+                    const friendNotification: Notification = {
+                        isSeen: false,
+                        description: `You are now friends with ${userFullName}.`,
+                        url: `/${userId}`,
+                        notifierUserId: userId,
+                        notifyRecieverUserId: friendId
+                    };
+
+                    // Notify both users
+                    notificationService.addNotification(userNotification);
+                    notificationService.addNotification(friendNotification);
 
                     // Delete the friend request
+                    request.ref.delete()
+                        .then(() => resolve())
+                        .catch(err => reject("Error occurred: " + err));
                 } else {
-                    console.log("Request doesn't exist!");
-                    // Create a new friend request
+                    // If the friend request didn't exist, create one
+                    // (ie if this user was the first to initiate a request)
                     request.ref.set({
+                        senderId: userId,
                         timestamp: moment().unix()
                     })
                     .then(() => resolve())
