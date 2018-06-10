1cc2606745057c7f87b7db32bc83e6b35c127203
Zachery Johnson <zacheryjohnson13@gmail.com>
2018-06-09 19:39:59 -0700

    Made the header title slightly more reliable

--- src/components/profileHeader/ProfileHeaderComponent.tsx
+++ src/components/profileHeader/ProfileHeaderComponent.tsx
@@ -1,19 +1,19 @@
 // - Import react components
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, {Component} from "react";
+import {connect} from "react-redux";
 import config from "src/config";
 import {Map} from "immutable";
 
 // - Material UI
-import { grey } from "material-ui/colors";
+import {grey} from "material-ui/colors";
 import IconButton from "material-ui/IconButton";
 import MoreVertIcon from "material-ui-icons/MoreVert";
-import { MenuList, MenuItem } from "material-ui/Menu";
+import {MenuList, MenuItem} from "material-ui/Menu";
 import Button from "material-ui/Button";
 import RaisedButton from "material-ui/Button";
-import EventListener, { withOptions } from "react-event-listener";
-import { Parallax, Background } from "react-parallax";
-import { getTranslate, getActiveLanguage } from "react-localize-redux";
+import EventListener, {withOptions} from "react-event-listener";
+import {Parallax, Background} from "react-parallax";
+import {getTranslate, getActiveLanguage} from "react-localize-redux";
 
 // - Import app components
 import ImgCover from "components/imgCover";
@@ -25,163 +25,168 @@ import UserAvatar from "components/userAvatar";
 // - Import actions
 import * as globalActions from "store/actions/globalActions";
 import * as userActions from "store/actions/userActions";
-import { IProfileHeaderComponentProps } from "./IProfileHeaderComponentProps";
-import { IProfileHeaderComponentState } from "./IProfileHeaderComponentState";
+import {IProfileHeaderComponentProps} from "./IProfileHeaderComponentProps";
+import {IProfileHeaderComponentState} from "./IProfileHeaderComponentState";
 
 /**
  * Create component class
  */
 export class ProfileHeaderComponent extends Component<IProfileHeaderComponentProps, IProfileHeaderComponentState> {
 
-    /**
-     * Component constructor
-     * @param  {object} props is an object properties of component
-     */
-  constructor (props: IProfileHeaderComponentProps) {
-    super(props);
+	/**
+	 * Component constructor
+	 * @param  {object} props is an object properties of component
+	 */
+	constructor(props: IProfileHeaderComponentProps) {
+		super(props);
 
-        /**
-         * Defaul state
-         */
-    this.state = {
-            /**
-             * If it's true , the window is in small size
-             */
-      isSmall: false
+		/**
+		 * Defaul state
+		 */
+		this.state = {
+			/**
+			 * If it's true , the window is in small size
+			 */
+			isSmall: false
 
-    };
+		};
 
-        // Binding functions to `this`
+		// Binding functions to `this`
 
-  }
-    /**
-     * Handle resize event for window to change sidebar status
-     * @param  {event} evt is the event is passed by winodw resize event
-     */
-  handleResize = () => {
+	}
 
-        // Set initial state
-    let width = window.innerWidth;
+	/**
+	 * Handle resize event for window to change sidebar status
+	 * @param  {event} evt is the event is passed by winodw resize event
+	 */
+	handleResize = () => {
 
-    if (width > 900) {
-      this.setState({
-        isSmall: false
-      });
+		// Set initial state
+		let width = window.innerWidth;
 
-    } else {
-      this.setState({
-        isSmall: true
-      });
-    }
-  }
+		if (width > 900) {
+			this.setState({
+				isSmall: false
+			});
 
-  componentDidMount () {
-    this.handleResize();
-  }
+		} else {
+			this.setState({
+				isSmall: true
+			});
+		}
+	}
 
-    /**
-     * Reneder component DOM
-     * @return {react element} return the DOM which rendered by component
-     */
-  render () {
-    const {translate, isAuthedUser, editProfileOpen} = this.props;
-    const styles = {
-      avatar: {
-        border: "2px solid rgb(255, 255, 255)"
-      },
-      iconButton: {
-        fill: "rgb(255, 255, 255)",
-        height: "24px",
-        width: "24px"
+	componentDidMount() {
+		this.handleResize();
+	}
 
-      },
-      iconButtonSmall: {
-        fill: "rgb(0, 0, 0)",
-        height: "24px",
-        width: "24px"
-      },
+	/**
+	 * Reneder component DOM
+	 * @return {react element} return the DOM which rendered by component
+	 */
+	render() {
+		const {translate, isAuthedUser, editProfileOpen} = this.props;
+		const styles = {
+			avatar: {
+				border: "2px solid rgb(255, 255, 255)"
+			},
+			iconButton: {
+				fill: "rgb(255, 255, 255)",
+				height: "24px",
+				width: "24px"
 
-      editButton: {
+			},
+			iconButtonSmall: {
+				fill: "rgb(0, 0, 0)",
+				height: "24px",
+				width: "24px"
+			},
 
-        marginLeft: "20px"
+			editButton: {
 
-      },
-      editButtonSmall: {
+				marginLeft: "20px"
 
-        marginLeft: "20px",
-        color: "white",
-        fill: "blue"
+			},
+			editButtonSmall: {
 
-      },
-      aboutButton: {
-        color: "white"
-      },
-      aboutButtonSmall: {
-        color: "black"
-      }
-    };
+				marginLeft: "20px",
+				color: "white",
+				fill: "blue"
 
-    const iconButtonElement = (
-            <IconButton style={this.state.isSmall ? styles.iconButtonSmall : styles.iconButton}>
-                <MoreVertIcon style={{...(this.state.isSmall ? styles.iconButtonSmall : styles.iconButton), color: grey[400]}} viewBox="10 0 24 24" />
-            </IconButton>
-        );
+			},
+			aboutButton: {
+				color: "white"
+			},
+			aboutButtonSmall: {
+				color: "black"
+			}
+		};
 
-    const RightIconMenu = () => (
-      <div>
-           {iconButtonElement}
-                <MenuItem style={{ fontSize: "14px" }}>Reply</MenuItem>
-                <MenuItem style={{ fontSize: "14px" }}>Edit</MenuItem>
-                <MenuItem style={{ fontSize: "14px" }}>Delete</MenuItem>
-      </div>
-        );
+		const iconButtonElement = (
+			<IconButton style={this.state.isSmall ? styles.iconButtonSmall : styles.iconButton}>
+				<MoreVertIcon
+					style={{...(this.state.isSmall ? styles.iconButtonSmall : styles.iconButton), color: grey[400]}}
+					viewBox="10 0 24 24"/>
+			</IconButton>
+		);
 
-    return (
+		const RightIconMenu = () => (
+			<div>
+				{iconButtonElement}
+				<MenuItem style={{fontSize: "14px"}}>Reply</MenuItem>
+				<MenuItem style={{fontSize: "14px"}}>Edit</MenuItem>
+				<MenuItem style={{fontSize: "14px"}}>Delete</MenuItem>
+			</div>
+		);
 
-            <div>
-                <Parallax strength={500} className="profile__parallax" bgStyle={{ position: "relative" }}>
-                    <Background>
-                        <ImgCover width="100%" height="510px" borderRadius="2px"
-                        fileName={this.props.banner || config.settings.defaultProfileCover} />
-                    </Background>
+		return (
 
-                </Parallax>
-                <div className={this.state.isSmall ? "profile__head-info-s" : "profile__head-info"}>
-                    <EventListener
-                        target="window"
-                        onResize={this.handleResize}
-                    />
-                    <div className="left">
-                        {/* User avatar*/}
-                        <div style={{ display: "flex", justifyContent: "center" }}><UserAvatar fullName={this.props.fullName || " "} fileName={this.props.avatar} size={60} style={styles.avatar} /></div>
-                        <div className="info">
-                            <div className="fullName">
-                                {this.props.fullName}
-                            </div>
-                            <div className="tagLine">
-                               {this.props.tagLine}
-                            </div>
-                            {/*<div className='followers'>
+			<div>
+				<Parallax strength={500} className="profile__parallax" bgStyle={{position: "relative"}}>
+					<Background>
+						<ImgCover width="100%" height="510px" borderRadius="2px"
+								  fileName={this.props.banner || config.settings.defaultProfileCover}/>
+					</Background>
+
+				</Parallax>
+				<div className={this.state.isSmall ? "profile__head-info-s" : "profile__head-info"}>
+					<EventListener
+						target="window"
+						onResize={this.handleResize}
+					/>
+					<div className="left">
+						{/* User avatar*/}
+						<div style={{display: "flex", justifyContent: "center"}}><UserAvatar
+							fullName={this.props.fullName || " "} fileName={this.props.avatar} size={60}
+							style={styles.avatar}/></div>
+						<div className="info">
+							<div className="fullName">
+								{this.props.fullName}
+							</div>
+							<div className="tagLine">
+								{this.props.tagLine}
+							</div>
+							{/*<div className='followers'>
                                 {this.props.followerCount} Followers
                 </div>*/}
-                        </div>
-                    </div>
-                    <div className="right">
-                        {isAuthedUser ? (<div style={this.state.isSmall ? styles.editButtonSmall : styles.editButton}>
-                        <Button variant="raised" onClick={this.props.openEditor}>
-                        {translate!("profile.editProfileButton")}
-                        </Button>
-                        </div>) : ""}
-                    </div>
-                </div>
-                {isAuthedUser && editProfileOpen ? (<EditProfile
-                    avatar={this.props.avatar}
-                    banner={this.props.banner}
-                    fullName={this.props.fullName}
-                />) : ""}
-            </div>
-    );
-  }
+						</div>
+					</div>
+					<div className="right">
+						{isAuthedUser ? (<div style={this.state.isSmall ? styles.editButtonSmall : styles.editButton}>
+							<Button variant="raised" onClick={this.props.openEditor}>
+								{translate!("profile.editProfileButton")}
+							</Button>
+						</div>) : ""}
+					</div>
+				</div>
+				{isAuthedUser && editProfileOpen ? (<EditProfile
+					avatar={this.props.avatar}
+					banner={this.props.banner}
+					fullName={this.props.fullName}
+				/>) : ""}
+			</div>
+		);
+	}
 }
 
 /**
@@ -191,9 +196,10 @@ export class ProfileHeaderComponent extends Component<IProfileHeaderComponentPro
  * @return {object}          props of component
  */
 const mapDispatchToProps = (dispatch: any, ownProps: IProfileHeaderComponentProps) => {
-  return {
-    openEditor: () => dispatch(userActions.openEditProfile())
-  };
+	return {
+		openEditor: () => dispatch(userActions.openEditProfile()),
+		setHeaderTitle: (title: string) => dispatch(globalActions.setHeaderTitle(title))
+	};
 };
 
 /**
@@ -204,10 +210,10 @@ const mapDispatchToProps = (dispatch: any, ownProps: IProfileHeaderComponentProp
  */
 const mapStateToProps = (state: Map<string, any>, ownProps: IProfileHeaderComponentProps) => {
 
-  return {
-    translate: getTranslate(state.get("locale")),
-    editProfileOpen: state.getIn(["user", "openEditProfile"])
-  };
+	return {
+		translate: getTranslate(state.get("locale")),
+		editProfileOpen: state.getIn(["user", "openEditProfile"])
+	};
 };
 
 // - Connect component to redux store
