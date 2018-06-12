5e045fc10051e3d14be37aa922f76180479c5515
Zachery Johnson <zacheryjohnson13@gmail.com>
2018-06-12 16:25:29 -0700

    Added vote tests

--- test/vote.test.ts
+++ test/vote.test.ts
@@ -1,57 +1,72 @@
 import "mocha";
 import * as chai from "chai";
+import chaiAsPromised from "chai-as-promised";
+
+chai.use(chaiAsPromised);
 chai.should();
 
 // Here we import whatever is needed for our tests
-import { Vote } from "../src/core/domain/votes";
-import * as voteActions from "../src/store/actions/voteActions";
-import { VoteActionType } from "../src/constants/voteActionType";
-import { Map } from "immutable";
+import {Vote} from "../src/core/domain/votes";
+import {VoteService} from "../src/data/firestoreClient/services/votes";
+import {Map} from "immutable";
 
-describe("Votes", function(){
-    // Create an empty vote to populate with data
-    let testVote: Vote = new Vote();
+describe("Votes", function () {
+	// Create an empty vote to populate with data
+	let testVote: Vote = new Vote();
 
-    // Create an empty object that will populate the vote component with info
-    let voteProps: any = {};
+	// Create an empty object that will populate the vote component with info
+	let voteProps: any = {};
 
-    // This runs before the suite is run. This is a good place to set up
-    // test data or pull from the database [also see beforeEach()]
-    before(function(){
-        const currentTime = Math.floor((new Date).getTime()/1000)
+	const voteService = new VoteService();
 
-        // Populate the data of a new vote
-        testVote.postId = "KthW1juoUKYUFs1B1mc84idbWly2";
-        testVote.creationDate = currentTime ; // This is in Unix epoch time
-        testVote.userDisplayName = "Zachary Johnson";
-        testVote.userId = "pretend_this_is_random";
-        testVote.userAvatar = "https://lh6.googleusercontent.com/-TOqPuJW4Ym4/AAAAAAAAAAI/AAAAAAAAAbM/NRGWb45ShXk/photo.jpg";
+	// This runs before the suite is run. This is a good place to set up
+	// test data or pull from the database [also see beforeEach()]
+	before(function () {
+		const currentTime = Math.floor((new Date).getTime() / 1000);
 
-        // Add the vote to a map, with it's key as the map ID
-        voteProps.vote = Map().set(testVote.postId, testVote);
-        voteProps.userAvatar = testVote.userAvatar;
-        voteProps.userId = testVote.userId;
-        voteProps.userDisplayName = testVote.userDisplayName;
-    });
+		// Populate the data of a new vote
+		testVote.postId = "SWJ0KoaqBwpX0Du70bZH"; // Spam Jeffrey with notifications
+		testVote.creationDate = currentTime; // This is in Unix epoch time
+		testVote.userDisplayName = "Zachery Johnson";
+		testVote.userId = "KthW1juoUKYUFs1B1mc84idbWly2";
+		testVote.userAvatar = "https://lh6.googleusercontent.com/-TOqPuJW4Ym4/AAAAAAAAAAI/AAAAAAAAAbM/NRGWb45ShXk/photo.jpg";
 
-    describe("#dbAddVote", function(){
-        it("should add a valid vote to the comment or post in the database with valid id", function(){
-        });
-        it("should error if comment or post has invalid post/comment id", function(){
-        });
-    });
-    
-    describe("#dbGetVotes", function(){
-        it("should get all votes from the database for the comment or post", function(){
-        });
-        it("should error if comment or post has invalid post/comment id", function(){
-        });
-    });
-    
-    describe("#dbDeleteVote", function(){
-        it("should delete a vote from the database for the comment or post", function(){
-        });
-        it("should error if comment or post has invalid post/comment id", function(){
-        });
-    });
+		// Add the vote to a map, with it's key as the map ID
+		voteProps.vote = Map().set(testVote.postId, testVote);
+		voteProps.userAvatar = testVote.userAvatar;
+		voteProps.userId = testVote.userId;
+		voteProps.userDisplayName = testVote.userDisplayName;
+	});
+
+	describe("Vote Service", function () {
+		this.slow(3000);
+
+		describe("#addVote", function () {
+			it("should add a vote to a valid post", function(){
+				return voteService.addVote(testVote).should.eventually.be.fulfilled;
+			});
+			it("should not add a vote to an invalid post", function(){
+				return voteService.addVote({}).should.eventually.be.rejected;
+			});
+		});
+		describe("#getVotes", function () {
+			it("should retrieve votes from a valid post", function(){
+				return voteService.getVotes(testVote.postId).should.eventually.be.fulfilled;
+			});
+			it("should not retrieve any votes from an invalid post", function(){
+				return voteService.getVotes("bad_post_id").should.eventually.be.rejected;
+			});
+		});
+		describe("#deleteVote", function () {
+			it("should remove a vote from a valid post", async function(){
+				// Add a vote to a post
+				await voteService.addVote(testVote);
+
+				// Delete the vote
+				await voteService.deleteVote(testVote.userId, testVote.postId);
+
+				return (await voteService.getVotes(testVote.postId)).should.not.have.property(testVote.userId);
+			});
+		});
+	});
 });
